46c6c11b1d00993d94cf88986525081ff0b32452
barbosayyy <72970256+barbosayyy@users.noreply.github.com>
2021-06-14 03:31:50 +0100

    v0.6.9.2

--- Assets/Scripts/AI/Guardian.cs
+++ Assets/Scripts/AI/Guardian.cs
@@ -6,13 +6,41 @@ using Panda;
 public class Guardian : MonoBehaviour
 {
     public GameObject player;
-    public Transform startPosition;
+    GameObject startPosObj;
     public Transform shootPoint;
+    public bool canSee;
+    public float speed;
+    public float rCmaxDist;
+    public float maxDistance;
+    public float distanceFromStart;
+
+    private void Start()
+    {
+        startPosObj = new GameObject();
+        startPosObj.transform.position = gameObject.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position, out RaycastHit hit, rCmaxDist) && hit.collider.CompareTag("Player"))
+        {
+            canSee = true;
+        }
+        else
+        {
+            canSee = false;
+        }
+    }
+
+    private void Update()
+    {
+        distanceFromStart = Vector3.Distance(gameObject.transform.position, startPosObj.transform.position);
+    }
 
     [Task]
     bool CanSeePlayer()
     {
-        if (player.active == true)
+        if (canSee == true)
         {
             return true;
         }
@@ -26,7 +54,11 @@ public class Guardian : MonoBehaviour
     [Task]
     void ApproachPlayer()
     {
-
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed);
+        if (distanceFromStart > maxDistance)
+        {
+            Task.current.Succeed();
+        }
     }
 
     [Task]
@@ -38,6 +70,10 @@ public class Guardian : MonoBehaviour
     [Task]
     void GoToStartPosition()
     {
-
+        transform.position = Vector3.MoveTowards(gameObject.transform.position, startPosObj.transform.position, speed);
+        if (gameObject.transform.position == startPosObj.transform.position)
+        {
+            Task.current.Succeed();
+        }
     }
 }
